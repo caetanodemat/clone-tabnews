3db20ce43ca050916698d54ce4f6011eab868a55
João Pedro Caetano de Matos <112180592+caetanodemat@users.noreply.github.com>
2026-08-03 22:17:13 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Leielly eu amo voce. Se voce me ama, da uma risadinha!</h1>
+    return <h1>Leielly eu amo voce. Se voce me ama, da uma risadinha! :)</h1>
 }
 export default Home;
